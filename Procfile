03db1628842e1620c686cf7fe55a72b334668d8e
Guilherme Pujoni <gpujoni.dev@gmail.com>
2020-12-03 17:45:10 -0300

    Mudancas com dotenv

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node -r dotenv/config --experimental-modules server.js
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import {userRouter} from './routes/userRouter.js'
 import mongoose from 'mongoose'
+require('dotenv').config()
 
 (async () => {
   try {
-    await mongoose.connect("mongodb+srv://GuiPujoni:343025gui@guipujonicluster.7lmmk.mongodb.net/project-stimulus-2020?retryWrites=true&w=majority", {
+    await mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@guipujonicluster.7lmmk.mongodb.net/project-stimulus-2020?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },);
@@ -19,6 +20,6 @@ app.use(express.json())
 app.use(userRouter)
 
 
-app.listen(3434, () =>{
+app.listen(process.env.PORT, () =>{
   console.log('Starter API Project Stimulus')
 })
